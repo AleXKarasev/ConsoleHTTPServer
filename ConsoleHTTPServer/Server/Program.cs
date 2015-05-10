6abd951f8d751212eb7eaea8bcab6336cc937784
AleXK <alexk.karasev@gmail.com>
2015-05-10 17:41:17 +0300

    Добавил конфигурирование порта из файла конфигурации

--- ConsoleHTTPServer/Server/Program.cs
+++ ConsoleHTTPServer/Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 
 namespace Server
 {
@@ -6,15 +7,24 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            // todo читаем порт из конфига
-            var port = 3000;
-
+            var port = GetPort();
             using (var server = new HttpServer(port))
             {
                 server.Start();
                 Console.WriteLine("Слушаем порт {0}. Для завершения работы нажмите Enter...", port.ToString());
                 Console.ReadLine();
             }
+        }
+
+        private static int GetPort()
+        {
+            string portConfig = ConfigurationManager.AppSettings["port"];
+            int port;
+            if (!Int32.TryParse(portConfig, out port))
+            {
+                port = 3000;
+            }
+            return port;
         }
     }
 }
